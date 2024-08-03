04dbc282484e8c49da3d5e954b25ac2d82495bdb
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-04 01:38:03 +0630

    Add project files.

---
+++ Modules.Auth.Api/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Modules.Auth.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
