e981f2d95addf12232f31df0a5e23da3eb3941b3
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-04 22:48:07 +0630

    [Added] Login Page

--- DotNet8.SingleDeviceLogin.App/Pages/Auth/P_Login.razor.cs
+++ DotNet8.SingleDeviceLogin.App/Pages/Auth/P_Login.razor.cs
@@ -1,6 +1,17 @@
-﻿namespace DotNet8.SingleDeviceLogin.App.Pages.Auth
+﻿using Shared.DTOs.Features;
+using Shared.DTOs.Features.Auth;
+
+namespace DotNet8.SingleDeviceLogin.App.Pages.Auth
 {
     public partial class P_Login
     {
+        private Result<JwtResponseModel> ResonseModel;
+
+        private LoginRequestModel RequestModel = new();
+
+        private async Task Login()
+        {
+
+        }
     }
 }
