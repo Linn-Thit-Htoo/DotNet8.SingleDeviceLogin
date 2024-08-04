4b44ad9ab8d20bd9691d12bd5dc5af3ed6e266ff
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-04 15:04:24 +0630

    [Added] Result Pattern

---
+++ Shared.DTOs/Features/Result.cs
@@ -0,0 +1,63 @@
+﻿using Shared.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.Features
+{
+    public class Result<T>
+    {
+        public T Data { get; set; }
+        public string Message { get; set; }
+        public EnumStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; }
+        public bool IsError => !IsSuccess;
+
+        public static Result<T> SuccessResult(string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
+        {
+            return new Result<T> { Message = message, StatusCode = statusCode, IsSuccess = true };
+        }
+
+        public static Result<T> SuccessResult(T data, string message = "Success.", EnumStatusCode statusCode = EnumStatusCode.Success)
+        {
+            return new Result<T> { Data = data, Message = message, StatusCode = statusCode, IsSuccess = true };
+        }
+
+        public static Result<T> SaveSuccessResult(string message = "Saving Successful.")
+        {
+            return Result<T>.SuccessResult(message);
+        }
+
+        public static Result<T> UpdateSuccessResult(string message = "Updating Successful.")
+        {
+            return Result<T>.SuccessResult(message);
+        }
+
+        public static Result<T> DeleteSuccessResult(string message = "Deleting Successful.")
+        {
+            return Result<T>.SuccessResult(message);
+        }
+
+        public static Result<T> FailureResult(string message = "Fail.", EnumStatusCode statusCode = EnumStatusCode.BadRequest)
+        {
+            return new Result<T> { Message = message, StatusCode = statusCode, IsSuccess = false };
+        }
+
+        public static Result<T> FailureResult(Exception ex)
+        {
+            return new Result<T> { Message = ex.ToString(), StatusCode = EnumStatusCode.InternalServerError, IsSuccess = false };
+        }
+
+        public static Result<T> NotFoundResult(string message = "No Data Found.")
+        {
+            return Result<T>.FailureResult(message, EnumStatusCode.NotFound);
+        }
+
+        public static Result<T> DuplicateResult(string message = "Duplicate Data.")
+        {
+            return Result<T>.FailureResult(message, EnumStatusCode.Conflict);
+        }
+    }
+}
