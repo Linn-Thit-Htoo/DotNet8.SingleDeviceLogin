5453995855351f8b9b1e9cc1cec066851113326a
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-04 18:08:47 +0630

    [Added] TokenValidationService

---
+++ Shard.Infrastructure/TokenValidationService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard.Infrastructure
+{
+    public class TokenValidationService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenValidationService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ClaimsPrincipal ValidateToken(string token)
+        {
+            try
+            {
+                JwtSecurityTokenHandler tokenHandler = new();
+                byte[] key = Encoding.ASCII.GetBytes(_configuration.GetSection("Jwt:Key").Value!);
+
+                TokenValidationParameters parameters =
+                    new()
+                    {
+                        RequireExpirationTime = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidAudience = _configuration["Jwt:Audience"],
+                        ValidIssuer = _configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                    };
+                ClaimsPrincipal principal = tokenHandler.ValidateToken(
+                    token,
+                    parameters,
+                    out SecurityToken securityToken
+                );
+
+                return principal;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
