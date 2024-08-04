f672899519e5b6bb3af1fc6121dfbc5ac28ec0a4
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-04 23:07:07 +0630

    [Added] Http Client Service

---
+++ DotNet8.SingleDeviceLogin.App/Services/HttpClientService.cs
@@ -0,0 +1,62 @@
+﻿using Shard.Infrastructure;
+using Shared.DTOs.Features;
+
+namespace DotNet8.SingleDeviceLogin.App.Services
+{
+	public class HttpClientService
+	{
+		private readonly HttpClient _httpClient;
+
+		public HttpClientService(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		public async Task<T> ExecuteAsync<T>(string endpoint, EnumHttpMethod httpMethod, object? requestModel = null)
+		{
+			HttpResponseMessage? response = null;
+			HttpContent? content = null;
+
+			if (requestModel is not null)
+			{
+				string jsonStr = requestModel.SerializeObject();
+				content = new StringContent(jsonStr);
+			}
+
+			switch (httpMethod)
+			{
+				case EnumHttpMethod.GET:
+					response = await _httpClient.GetAsync(endpoint);
+					break;
+				case EnumHttpMethod.POST:
+					response = await _httpClient.PostAsync(endpoint, content);
+					break;
+				case EnumHttpMethod.PUT:
+					response = await _httpClient.PutAsync(endpoint, content);
+					break;
+				case EnumHttpMethod.PATCH:
+					response = await _httpClient.PatchAsync(endpoint, content);
+					break;
+				case EnumHttpMethod.DELETE:
+					response = await _httpClient.DeleteAsync(endpoint);
+					break;
+				case EnumHttpMethod.None:
+				default:
+					break;
+			}
+
+			string jsonResponse = await response!.Content.ReadAsStringAsync();
+			return jsonResponse.DeserializeObject<T>();
+		}
+	}
+
+	public enum EnumHttpMethod
+	{
+		None,
+		GET,
+		POST,
+		PUT,
+		PATCH,
+		DELETE,
+	}
+}
--- Shard.Infrastructure/DevCode.cs
+++ Shard.Infrastructure/DevCode.cs
@@ -2,23 +2,28 @@
 
 namespace Shard.Infrastructure
 {
-    public static class DevCode
-    {
-        public static string SerializeObject(this object obj)
-        {
-            return JsonConvert.SerializeObject(obj);
-        }
+	public static class DevCode
+	{
+		public static string SerializeObject(this object obj)
+		{
+			return JsonConvert.SerializeObject(obj);
+		}
 
-        public static string GetCurrentMyanmarDateTime()
-        {
-            var myanmarTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Myanmar Standard Time");
-            var myanmarDateTime = TimeZoneInfo.ConvertTime(
-                DateTime.Now,
-                TimeZoneInfo.Local,
-                myanmarTimeZone
-            );
+		public static T DeserializeObject<T>(this string jsonStr)
+		{
+			return JsonConvert.DeserializeObject<T>(jsonStr)!;
+		}
 
-            return myanmarDateTime.ToString("yyyy-MM-dd HH:mm:ss");
-        }
-    }
+		public static string GetCurrentMyanmarDateTime()
+		{
+			var myanmarTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Myanmar Standard Time");
+			var myanmarDateTime = TimeZoneInfo.ConvertTime(
+				DateTime.Now,
+				TimeZoneInfo.Local,
+				myanmarTimeZone
+			);
+
+			return myanmarDateTime.ToString("yyyy-MM-dd HH:mm:ss");
+		}
+	}
 }
