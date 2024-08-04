9c17f78a614d0b1a2de7d9cd7d27e2550fa0b0d4
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-04 15:36:26 +0630

    [Added] Auth Extension

---
+++ Modules.Auth.Api/Extensions/DependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Modules.Auth.Application.Services;
+using Modules.Auth.Domain.Interfaces;
+using Modules.Auth.Infrastructure.Db;
+
+namespace Modules.Auth.Api.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            return services.AddDbContextService(builder)
+                .AddAuthService()
+                .AddValidatorService();
+        }
+
+        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<AuthDbContext>(opt =>
+            {
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddAuthService(this IServiceCollection services)
+        {
+            return services.AddScoped<IAuthService, IAuthService>();
+        }
+
+        private static IServiceCollection AddValidatorService(this IServiceCollection services)
+        {
+            return services.AddScoped<RegisterValidator>();
+        }
+    }
+}
