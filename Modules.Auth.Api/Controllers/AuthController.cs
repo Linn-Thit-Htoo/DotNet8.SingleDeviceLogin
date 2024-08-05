db50d34c4415de0dd6a672c84c64ab7242de1be4
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:04:16 +0630

    Format Code

--- Modules.Auth.Api/Controllers/AuthController.cs
+++ Modules.Auth.Api/Controllers/AuthController.cs
@@ -1,43 +1,42 @@
-﻿namespace Modules.Auth.Api.Controllers
+﻿namespace Modules.Auth.Api.Controllers;
+
+[Route("api/account")]
+[ApiController]
+public class AuthController : BaseController
 {
-    [Route("api/account")]
-    [ApiController]
-    public class AuthController : BaseController
+    private readonly IAuthService _authService;
+    private readonly IMediator _mediator;
+
+    public AuthController(IAuthService authService, IMediator mediator)
     {
-        private readonly IAuthService _authService;
-        private readonly IMediator _mediator;
+        _authService = authService;
+        _mediator = mediator;
+    }
 
-        public AuthController(IAuthService authService, IMediator mediator)
-        {
-            _authService = authService;
-            _mediator = mediator;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetUserList()
+    {
+        var query = new GetUserListQuery();
+        var result = await _mediator.Send(query);
 
-        [HttpGet]
-        public async Task<IActionResult> GetUserList()
-        {
-            var query = new GetUserListQuery();
-            var result = await _mediator.Send(query);
+        return Content(result);
+    }
 
-            return Content(result);
-        }
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequestModel requestModel)
+    {
+        var command = new RegisterCommand(requestModel);
+        var result = await _mediator.Send(command);
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterRequestModel requestModel)
-        {
-            var command = new RegisterCommand(requestModel);
-            var result = await _mediator.Send(command);
+        return Content(result);
+    }
 
-            return Content(result);
-        }
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestModel requestModel)
+    {
+        var query = new LoginQuery(requestModel);
+        var result = await _mediator.Send(query);
 
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginRequestModel requestModel)
-        {
-            var query = new LoginQuery(requestModel);
-            var result = await _mediator.Send(query);
-
-            return Content(result);
-        }
+        return Content(result);
     }
 }
