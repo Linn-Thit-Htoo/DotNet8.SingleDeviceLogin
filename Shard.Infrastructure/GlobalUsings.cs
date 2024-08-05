34ba1381d4d3814d273d2d930126249b56c4fa1b
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:13:05 +0630

    Format Code

--- Shard.Infrastructure/GlobalUsings.cs
+++ Shard.Infrastructure/GlobalUsings.cs
@@ -1,6 +1,9 @@
 // Global using directives
 
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
 global using System.Security.Cryptography;
 global using System.Text;
 global using Microsoft.Extensions.Configuration;
+global using Microsoft.IdentityModel.Tokens;
 global using Newtonsoft.Json;
--- Shard.Infrastructure/TokenValidationService.cs
+++ Shard.Infrastructure/TokenValidationService.cs
@@ -1,52 +1,43 @@
-﻿using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿namespace Shard.Infrastructure;
 
-namespace Shard.Infrastructure
+public class TokenValidationService
 {
-    public class TokenValidationService
+    private readonly IConfiguration _configuration;
+
+    public TokenValidationService(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        public TokenValidationService(IConfiguration configuration)
+    public ClaimsPrincipal ValidateToken(string token)
+    {
+        try
         {
-            _configuration = configuration;
+            JwtSecurityTokenHandler tokenHandler = new();
+            byte[] key = Encoding.ASCII.GetBytes(_configuration.GetSection("Jwt:Key").Value!);
+
+            TokenValidationParameters parameters =
+                new()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidAudience = _configuration["Jwt:Audience"],
+                    ValidIssuer = _configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            ClaimsPrincipal principal = tokenHandler.ValidateToken(
+                token,
+                parameters,
+                out SecurityToken securityToken
+            );
+
+            return principal;
         }
-
-        public ClaimsPrincipal ValidateToken(string token)
+        catch (Exception ex)
         {
-            try
-            {
-                JwtSecurityTokenHandler tokenHandler = new();
-                byte[] key = Encoding.ASCII.GetBytes(_configuration.GetSection("Jwt:Key").Value!);
-
-                TokenValidationParameters parameters =
-                    new()
-                    {
-                        RequireExpirationTime = true,
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidAudience = _configuration["Jwt:Audience"],
-                        ValidIssuer = _configuration["Jwt:Issuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(key)
-                    };
-                ClaimsPrincipal principal = tokenHandler.ValidateToken(
-                    token,
-                    parameters,
-                    out SecurityToken securityToken
-                );
-
-                return principal;
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
+            throw;
         }
     }
 }
