3ec3b0d279df8f31707f32a38c9b2a5226b079cf
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:07:15 +0630

    Format Code

--- Modules.Auth.Application/Features/Auth/Login/LoginQuery.cs
+++ Modules.Auth.Application/Features/Auth/Login/LoginQuery.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Modules.Auth.Application.Features.Auth.Login;
 
-namespace Modules.Auth.Application.Features.Auth.Login
+public class LoginQuery : IRequest<Result<JwtResponseModel>>
 {
-    public class LoginQuery : IRequest<Result<JwtResponseModel>>
-    {
-        public LoginRequestModel RequestModel { get; set; }
+    public LoginRequestModel RequestModel { get; set; }
 
-        public LoginQuery(LoginRequestModel requestModel)
-        {
-            RequestModel = requestModel;
-        }
+    public LoginQuery(LoginRequestModel requestModel)
+    {
+        RequestModel = requestModel;
     }
 }
