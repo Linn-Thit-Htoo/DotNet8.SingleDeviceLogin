73f5e1ae4d147a848a732b0fce9349e82ee7ca19
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:12:37 +0630

    Format Code

--- Shard.Infrastructure/AesService.cs
+++ Shard.Infrastructure/AesService.cs
@@ -1,86 +1,85 @@
-﻿namespace Shard.Infrastructure
+﻿namespace Shard.Infrastructure;
+
+public class AesService
 {
-    public class AesService
+    private readonly IConfiguration _configuration;
+
+    public AesService(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        public AesService(IConfiguration configuration)
+    #region Encryt Service
+
+    public string EncryptString(string raw)
+    {
+        byte[] iv;
+        byte[] encrypted;
+
+        using (Aes aes = Aes.Create())
         {
-            _configuration = configuration;
-        }
+            aes.Key = Encoding.UTF8.GetBytes(
+                Environment.GetEnvironmentVariable("NK_EncryptionKey")!
+            );
+            aes.GenerateIV();
+            iv = aes.IV;
 
-        #region Encryt Service
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
 
-        public string EncryptString(string raw)
-        {
-            byte[] iv;
-            byte[] encrypted;
-
-            using (Aes aes = Aes.Create())
+            using (MemoryStream memoryStream = new MemoryStream())
             {
-                aes.Key = Encoding.UTF8.GetBytes(
-                    Environment.GetEnvironmentVariable("NK_EncryptionKey")!
-                );
-                aes.GenerateIV();
-                iv = aes.IV;
-
-                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-                using (MemoryStream memoryStream = new MemoryStream())
-                {
-                    using (
-                        CryptoStream cryptoStream = new CryptoStream(
-                            memoryStream,
-                            encryptor,
-                            CryptoStreamMode.Write
-                        )
+                using (
+                    CryptoStream cryptoStream = new CryptoStream(
+                        memoryStream,
+                        encryptor,
+                        CryptoStreamMode.Write
                     )
+                )
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
                     {
-                        using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
-                        {
-                            streamWriter.Write(raw);
-                        }
-                        encrypted = memoryStream.ToArray();
+                        streamWriter.Write(raw);
                     }
+                    encrypted = memoryStream.ToArray();
                 }
             }
-
-            byte[] result = new byte[iv.Length + encrypted.Length];
-            Array.Copy(iv, 0, result, 0, iv.Length);
-            Array.Copy(encrypted, 0, result, iv.Length, encrypted.Length);
-
-            return Convert.ToBase64String(result);
         }
 
-        #endregion
+        byte[] result = new byte[iv.Length + encrypted.Length];
+        Array.Copy(iv, 0, result, 0, iv.Length);
+        Array.Copy(encrypted, 0, result, iv.Length, encrypted.Length);
 
-        #region Decrypt Service
-
-        public string DecryptString(string encryptedText)
-        {
-            byte[] fullCipher = Convert.FromBase64String(encryptedText);
-
-            byte[] iv = new byte[16];
-            byte[] cipherText = new byte[fullCipher.Length - iv.Length];
-
-            Array.Copy(fullCipher, 0, iv, 0, iv.Length);
-            Array.Copy(fullCipher, iv.Length, cipherText, 0, cipherText.Length);
-
-            using Aes aes = Aes.Create();
-            aes.Key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("NK_EncryptionKey")!);
-            aes.IV = iv;
-            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-
-            using MemoryStream memoryStream = new MemoryStream(cipherText);
-            using CryptoStream cryptoStream = new CryptoStream(
-                memoryStream,
-                decryptor,
-                CryptoStreamMode.Read
-            );
-            using StreamReader streamReader = new StreamReader(cryptoStream);
-            return streamReader.ReadToEnd();
-        }
-
-        #endregion
+        return Convert.ToBase64String(result);
     }
+
+    #endregion
+
+    #region Decrypt Service
+
+    public string DecryptString(string encryptedText)
+    {
+        byte[] fullCipher = Convert.FromBase64String(encryptedText);
+
+        byte[] iv = new byte[16];
+        byte[] cipherText = new byte[fullCipher.Length - iv.Length];
+
+        Array.Copy(fullCipher, 0, iv, 0, iv.Length);
+        Array.Copy(fullCipher, iv.Length, cipherText, 0, cipherText.Length);
+
+        using Aes aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("NK_EncryptionKey")!);
+        aes.IV = iv;
+        ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+        using MemoryStream memoryStream = new MemoryStream(cipherText);
+        using CryptoStream cryptoStream = new CryptoStream(
+            memoryStream,
+            decryptor,
+            CryptoStreamMode.Read
+        );
+        using StreamReader streamReader = new StreamReader(cryptoStream);
+        return streamReader.ReadToEnd();
+    }
+
+    #endregion
 }
