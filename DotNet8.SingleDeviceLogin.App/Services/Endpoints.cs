59f82b62e064e164ff73c01c0a7200ffc6d8e3d2
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:02:02 +0630

    Format Code

--- DotNet8.SingleDeviceLogin.App/Services/Endpoints.cs
+++ DotNet8.SingleDeviceLogin.App/Services/Endpoints.cs
@@ -1,8 +1,7 @@
-﻿namespace DotNet8.SingleDeviceLogin.App.Services
+﻿namespace DotNet8.SingleDeviceLogin.App.Services;
+
+public class Endpoints
 {
-    public class Endpoints
-    {
-        public static string Login { get; } = "/api/account/login";
-        public static string UserList { get; } = "/api/account";
-    }
+    public static string Login { get; } = "/api/account/login";
+    public static string UserList { get; } = "/api/account";
 }
