0a7bb292cc090f764b5cde44634e7b1c06e82892
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:10:10 +0630

    Format Code

--- Modules.Auth.Domain/Entities/Tbl_User.cs
+++ Modules.Auth.Domain/Entities/Tbl_User.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Modules.Auth.Domain.Entities;
 
-namespace Modules.Auth.Domain.Entities
+[Table("Tbl_User")]
+public class Tbl_User
 {
-    [Table("Tbl_User")]
-    public class Tbl_User
-    {
-        [Key]
-        public string UserId { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string UserRole { get; set; }
-        public bool IsActive { get; set; }
-    }
+    [Key]
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string UserRole { get; set; }
+    public bool IsActive { get; set; }
 }
