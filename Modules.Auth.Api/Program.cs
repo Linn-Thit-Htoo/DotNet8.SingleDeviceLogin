2bdbf2b7da792cacac0e3877d35110decfc283ac
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:05:36 +0630

    Format Code

--- Modules.Auth.Api/Program.cs
+++ Modules.Auth.Api/Program.cs
@@ -1,11 +1,9 @@
-using Modules.Auth.Api.Extensions;
-using Modules.Auth.Application.Extensions;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
