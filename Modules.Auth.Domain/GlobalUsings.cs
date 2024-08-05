51579aa9ebda3d6b2ae09db41e47621ee0f528d3
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:10:50 +0630

    Format Code

--- Modules.Auth.Domain/GlobalUsings.cs
+++ Modules.Auth.Domain/GlobalUsings.cs
@@ -3,3 +3,5 @@
 global using System.ComponentModel;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
+global using Shared.DTOs.Features;
+global using Shared.DTOs.Features.Auth;
--- Modules.Auth.Domain/Interfaces/IAuthService.cs
+++ Modules.Auth.Domain/Interfaces/IAuthService.cs
@@ -1,17 +1,14 @@
-﻿using Shared.DTOs.Features;
-using Shared.DTOs.Features.Auth;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Modules.Auth.Domain.Interfaces;
 
-namespace Modules.Auth.Domain.Interfaces
+public interface IAuthService
 {
-    public interface IAuthService
-    {
-        Task<Result<RegisterResponseModel>> Register(RegisterRequestModel requestModel, CancellationToken cancellationToken);
-        Task<Result<JwtResponseModel>> Login(LoginRequestModel requestModel, CancellationToken cancellationToken);
-        Task<Result<UserListResponseModel>> GetUserList(CancellationToken cancellationToken);
-    }
+    Task<Result<RegisterResponseModel>> Register(
+        RegisterRequestModel requestModel,
+        CancellationToken cancellationToken
+    );
+    Task<Result<JwtResponseModel>> Login(
+        LoginRequestModel requestModel,
+        CancellationToken cancellationToken
+    );
+    Task<Result<UserListResponseModel>> GetUserList(CancellationToken cancellationToken);
 }
