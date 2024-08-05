5fce3db38f77634d6abf847f03f91ddb8f2e142a
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:11:24 +0630

    Format Code

--- Modules.Auth.Infrastructure/Db/AuthDbContext.cs
+++ Modules.Auth.Infrastructure/Db/AuthDbContext.cs
@@ -2,9 +2,8 @@
 
 public class AuthDbContext : DbContext
 {
-    public AuthDbContext(DbContextOptions options) : base(options)
-    {
-    }
+    public AuthDbContext(DbContextOptions options)
+        : base(options) { }
 
     public DbSet<Tbl_User> Tbl_Users { get; set; }
     public DbSet<Tbl_Login> Tbl_Logins { get; set; }
