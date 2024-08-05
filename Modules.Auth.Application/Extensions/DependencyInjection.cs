1457785cf7bafce49ce316fd73d5d7da20dedd32
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:06:22 +0630

    [Added] Global Usings in Application

--- Modules.Auth.Application/Extensions/DependencyInjection.cs
+++ Modules.Auth.Application/Extensions/DependencyInjection.cs
@@ -1,17 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Modules.Auth.Application.Extensions;
 
-namespace Modules.Auth.Application.Extensions
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
-        {
-            return services.AddMediatR(cf => cf.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
-        }
+        return services.AddMediatR(cf =>
+            cf.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly)
+        );
     }
 }
