7cec6db6bf7abb3318a1c797d42c06d5e186b0c6
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:08:23 +0630

    Format Code

--- Modules.Auth.Application/GlobalUsings.cs
+++ Modules.Auth.Application/GlobalUsings.cs
@@ -1,7 +1,12 @@
 // Global using directives
 
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
 global using MediatR;
+global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.IdentityModel.Tokens;
 global using Modules.Auth.Application.Services.ValidatorServices;
 global using Modules.Auth.Domain.Interfaces;
 global using Shared.Domain.Enums;
--- Modules.Auth.Application/Services/AuthService.cs
+++ Modules.Auth.Application/Services/AuthService.cs
@@ -6,7 +6,6 @@ using Shard.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Modules.Auth.Application.Services
