e2e950250317bf3c802f7922e615c4eea26f4242
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:01:47 +0630

    Format Code

--- DotNet8.SingleDeviceLogin.App/Pages/Auth/P_UserList.razor.cs
+++ DotNet8.SingleDeviceLogin.App/Pages/Auth/P_UserList.razor.cs
@@ -1,26 +1,28 @@
-﻿namespace DotNet8.SingleDeviceLogin.App.Pages.Auth
-{
-    public partial class P_UserList
-    {
-        private Result<UserListResponseModel> ResponseModel = new();
+﻿namespace DotNet8.SingleDeviceLogin.App.Pages.Auth;
 
-        protected override async Task OnInitializedAsync()
+public partial class P_UserList
+{
+    private Result<UserListResponseModel> ResponseModel = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        await List();
+    }
+
+    private async Task List()
+    {
+        ResponseModel = await HttpClientService.ExecuteAsync<Result<UserListResponseModel>>(
+            Endpoints.UserList,
+            EnumHttpMethod.GET
+        );
+        if (ResponseModel.IsError)
         {
-            await List();
+            Snackbar.Add(ResponseModel.Message, Severity.Error);
         }
 
-        private async Task List()
+        if (ResponseModel.StatusCode == EnumStatusCode.UnAuthorized)
         {
-            ResponseModel = await HttpClientService.ExecuteAsync<Result<UserListResponseModel>>(Endpoints.UserList, EnumHttpMethod.GET);
-            if (ResponseModel.IsError)
-            {
-                Snackbar.Add(ResponseModel.Message, Severity.Error);
-            }
-
-            if (ResponseModel.StatusCode == EnumStatusCode.UnAuthorized)
-            {
-                Navigation.NavigateTo("/");
-            }
+            Navigation.NavigateTo("/");
         }
     }
 }
