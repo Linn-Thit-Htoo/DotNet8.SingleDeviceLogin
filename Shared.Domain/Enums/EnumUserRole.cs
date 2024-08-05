c5d3e53b96b8be4fe8ce9944b510126a6b0a5ae0
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:13:35 +0630

    Format Code

--- Shared.Domain/Enums/EnumUserRole.cs
+++ Shared.Domain/Enums/EnumUserRole.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Shared.Domain.Enums;
 
-namespace Shared.Domain.Enums
+public enum EnumUserRole
 {
-    public enum EnumUserRole
-    {
-        None,
-        Admin,
-        User
-    }
+    None,
+    Admin,
+    User
 }
