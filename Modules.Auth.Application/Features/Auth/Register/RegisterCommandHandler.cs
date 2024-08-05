dd2abbbc7785bf183edd9db0f38c507a47c63f4b
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:07:58 +0630

    Format Code

--- Modules.Auth.Application/Features/Auth/Register/RegisterCommandHandler.cs
+++ Modules.Auth.Application/Features/Auth/Register/RegisterCommandHandler.cs
@@ -1,51 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Shared.Domain.Enums;
+﻿namespace Modules.Auth.Application.Features.Auth.Register;
 
-namespace Modules.Auth.Application.Features.Auth.Register
+public class RegisterCommandHandler
+    : IRequestHandler<RegisterCommand, Result<RegisterResponseModel>>
 {
-    public class RegisterCommandHandler : IRequestHandler<RegisterCommand, Result<RegisterResponseModel>>
+    private readonly IAuthService _authService;
+    private readonly RegisterValidator _registerValidator;
+
+    public RegisterCommandHandler(IAuthService authService, RegisterValidator registerValidator)
     {
-        private readonly IAuthService _authService;
-        private readonly RegisterValidator _registerValidator;
+        _authService = authService;
+        _registerValidator = registerValidator;
+    }
 
-        public RegisterCommandHandler(IAuthService authService, RegisterValidator registerValidator)
+    public async Task<Result<RegisterResponseModel>> Handle(
+        RegisterCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        Result<RegisterResponseModel> responseModel;
+        try
         {
-            _authService = authService;
-            _registerValidator = registerValidator;
+            var validationResult = await _registerValidator.ValidateAsync(request.RequestModel);
+            if (!validationResult.IsValid)
+            {
+                string errors = string.Join(
+                    " ",
+                    validationResult.Errors.Select(x => x.ErrorMessage)
+                );
+                responseModel = Result<RegisterResponseModel>.FailureResult(errors);
+                goto result;
+            }
+
+            if (!Enum.IsDefined(typeof(EnumUserRole), request.RequestModel.UserRole))
+            {
+                responseModel = Result<RegisterResponseModel>.FailureResult("Invalid User Role.");
+                goto result;
+            }
+
+            responseModel = await _authService.Register(request.RequestModel, cancellationToken);
         }
-
-        public async Task<Result<RegisterResponseModel>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        catch (Exception ex)
         {
-            Result<RegisterResponseModel> responseModel;
-            try
-            {
-                var validationResult = await _registerValidator.ValidateAsync(request.RequestModel);
-                if (!validationResult.IsValid)
-                {
-                    string errors = string.Join(" ", validationResult.Errors.Select(x => x.ErrorMessage));
-                    responseModel = Result<RegisterResponseModel>.FailureResult(errors);
-                    goto result;
-                }
-
-                if (!Enum.IsDefined(typeof(EnumUserRole), request.RequestModel.UserRole))
-                {
-                    responseModel = Result<RegisterResponseModel>.FailureResult("Invalid User Role.");
-                    goto result;
-                }
-
-                responseModel = await _authService.Register(request.RequestModel, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                responseModel = Result<RegisterResponseModel>.FailureResult(ex);
-            }
+            responseModel = Result<RegisterResponseModel>.FailureResult(ex);
+        }
 
         result:
-            return responseModel;
-        }
+        return responseModel;
     }
 }
--- Modules.Auth.Application/GlobalUsings.cs
+++ Modules.Auth.Application/GlobalUsings.cs
@@ -4,5 +4,6 @@ global using MediatR;
 global using Microsoft.Extensions.DependencyInjection;
 global using Modules.Auth.Application.Services.ValidatorServices;
 global using Modules.Auth.Domain.Interfaces;
+global using Shared.Domain.Enums;
 global using Shared.DTOs.Features;
 global using Shared.DTOs.Features.Auth;
