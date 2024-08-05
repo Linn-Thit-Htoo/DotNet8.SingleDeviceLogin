80ccbeb7eee7e1cc0cf09469b5be70a5226d48e2
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:07:46 +0630

    Format Code

--- Modules.Auth.Application/Features/Auth/Register/RegisterCommand.cs
+++ Modules.Auth.Application/Features/Auth/Register/RegisterCommand.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Modules.Auth.Application.Features.Auth.Register;
 
-namespace Modules.Auth.Application.Features.Auth.Register
+public class RegisterCommand : IRequest<Result<RegisterResponseModel>>
 {
-    public class RegisterCommand : IRequest<Result<RegisterResponseModel>>
-    {
-        public RegisterRequestModel RequestModel { get; set; }
+    public RegisterRequestModel RequestModel { get; set; }
 
-        public RegisterCommand(RegisterRequestModel requestModel)
-        {
-            RequestModel = requestModel;
-        }
+    public RegisterCommand(RegisterRequestModel requestModel)
+    {
+        RequestModel = requestModel;
     }
 }
