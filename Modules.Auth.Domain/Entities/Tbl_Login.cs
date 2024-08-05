e07e4d8a4676bf500dd8c32b906e530bb11306fd
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:10:01 +0630

    Format Code

--- Modules.Auth.Domain/Entities/Tbl_Login.cs
+++ Modules.Auth.Domain/Entities/Tbl_Login.cs
@@ -1,12 +1,11 @@
-﻿namespace Modules.Auth.Domain.Entities
+﻿namespace Modules.Auth.Domain.Entities;
+
+[Table("Tbl_Login")]
+public class Tbl_Login
 {
-    [Table("Tbl_Login")]
-    public class Tbl_Login
-    {
-        [Key]
-        public string LoginId { get; set; }
-        public string Email { get; set; }
-        public string Token { get; set; }
-        public string CreatedDate { get; set; }
-    }
+    [Key]
+    public string LoginId { get; set; }
+    public string Email { get; set; }
+    public string Token { get; set; }
+    public string CreatedDate { get; set; }
 }
