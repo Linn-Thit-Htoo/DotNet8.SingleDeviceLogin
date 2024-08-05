b987496658ca7ef22e32a40c84d184c51d3dcff5
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-05 09:03:39 +0630

    [Added] Global Using in Api

---
+++ Modules.Auth.Api/GlobalUsings.cs
@@ -0,0 +1,9 @@
+// Global using directives
+
+global using MediatR;
+global using Microsoft.AspNetCore.Mvc;
+global using Modules.Auth.Application.Features.Auth.GetUserList;
+global using Modules.Auth.Application.Features.Auth.Login;
+global using Modules.Auth.Application.Features.Auth.Register;
+global using Modules.Auth.Domain.Interfaces;
+global using Shared.DTOs.Features.Auth;
